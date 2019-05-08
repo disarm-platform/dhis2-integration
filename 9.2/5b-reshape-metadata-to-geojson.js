b90246e3c06b276ef39563a419bdd22cbd67ef45
Jonathan Smith <jonathan@peoplesized.com>
2019-05-08 17:18:23 +0200

    needs jq, but works in one-ish line

--- 9.2/5b-reshape-metadata-to-geojson.js
+++
@@ -1,1 +0,0 @@
-// Is a weird format. Needs to be passed to DiSARM as a GeoJSON FeatureCollection. Hello turf.js?
---
+++ 9.2/5b-reshape-metadata-to-geojson.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Is a weird format. Needs to be passed to DiSARM as a GeoJSON FeatureCollection. Hello turf.js?
+
+# Start with `metajson.json`
+cat metadata.json |
+  jq \
+    '{type: "FeatureCollection", features: [.organisationUnits[] |
+    {"type": "Feature", "properties": {"id": .id},
+    "geometry": {"type": "Polygon","coordinates": 
+    (.coordinates | sub("^\\["; "") | sub("\\]$"; "") | fromjson)  }} ]}'
