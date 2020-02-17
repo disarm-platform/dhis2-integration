5804d19aebd15dbda41c382f8dfb0774a5bd8ca4
Jonathan Smith <jonathan@peoplesized.com>
2020-02-17 15:06:17 +0200

    split out main - easier to test?

--- 2020-disarm-api/cloud-function/src/index.ts
+++ 2020-disarm-api/cloud-function/src/index.ts
@@ -7,18 +7,7 @@
  * The `dhis2` file contains functionality that reads and writes data from DHIS2
  */
 import express from 'express';
-import { prepare_request_for_disarm, run_disarm_algorithm, shape_result_for_dhis2 } from './disarm';
-import { get_data_from_dhis2, write_result_to_dhis2, trigger_dhis2_analytics } from './dhis2';
-
-async function main(): Promise<boolean> {
-  const { dataValueSets, orgUnitsFeatures, dataElementLookup } = await get_data_from_dhis2();
-  const fn_request = await prepare_request_for_disarm(dataValueSets, orgUnitsFeatures, dataElementLookup);
-  const run_result = await run_disarm_algorithm(fn_request);
-  const data_for_dhis2 = await shape_result_for_dhis2(run_result, dataElementLookup);
-  await write_result_to_dhis2(data_for_dhis2);
-  await trigger_dhis2_analytics();
-  return true;
-}
+import { main } from './main';
 
 exports.handler = async (req: express.Request, res: express.Response) => {
   res.set('Access-Control-Allow-Origin', '*');
